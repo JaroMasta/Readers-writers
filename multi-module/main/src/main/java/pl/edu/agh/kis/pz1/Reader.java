7aa23ef761a82851c8015dea9d6800d3d9a971ea
JaroMasta <147257065+JaroMasta@users.noreply.github.com>
2025-01-16 18:46:13 +0100

    Added changes, switched logic to notifyAll and wait usage, delete method start() in library class

--- multi-module/main/src/main/java/pl/edu/agh/kis/pz1/Reader.java
+++ multi-module/main/src/main/java/pl/edu/agh/kis/pz1/Reader.java
@@ -1,31 +1,25 @@
 package pl.edu.agh.kis.pz1;
 
-public class Reader extends Visitor {
+public class Reader extends Visitor implements Runnable {
     private final Library library;
 
     public Reader(Library library, String name) {
-        super(name, 0);
+        super(name);
         this.library = library;
     }
 
-    @Override
     public void run() {
         try {
-            while (!Thread.currentThread().isInterrupted()) {
+            while(true) {
                 System.out.println(name() + " chce czytać.");
                 library.requestReading(this);
-                while(status != 1 && !Thread.currentThread().isInterrupted()) {
-                   Thread.sleep((long) 50); // Symulacja czekania na czytanie
-                }
-
                 library.startReading(this);
-                Thread.sleep((long) 3000); // Symulacja czytania (1-3 sekundy)
+                Thread.sleep((long) (Math.random() * 3000)); // Symulacja czytania (1-3 sekundy)
                 library.finishReading(this);
-                this.setStatus(0);
             }
         } catch (InterruptedException e) {
-            System.out.println(getName() + " został przerwany.");
-            Thread.currentThread().interrupt();
+            System.out.println(name() + " został przerwany.");
         }
     }
+
 }
--- multi-module/main/src/main/java/pl/edu/agh/kis/pz1/Visitor.java
+++ multi-module/main/src/main/java/pl/edu/agh/kis/pz1/Visitor.java
@@ -1,20 +1,13 @@
 package pl.edu.agh.kis.pz1;
 
-public abstract class Visitor extends Thread{
+public abstract class Visitor{
     protected int status;
     String name;
-    protected Visitor(String name, int status) { // 1 for in library 0 for not in library
+    protected Visitor(String name) { // 1 for in library 0 for not in library
         this.name = name;
-        this.status = status;
-    }
-    public void setStatus(int status) {
-        this.status = status;
     }
     public String name(){
         return this.name;
     }
-    @Override
-    public void run() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+
 }
